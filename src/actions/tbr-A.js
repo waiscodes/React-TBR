f85905bbd90b07579827033f21f51756a47a8257
waiscodes <b.wais@outlook.com>
2020-09-16 18:30:08 -0600

    added a dark mode action

---
+++ src/actions/tbr-A.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const DarkMode = () => {
+  return {
+    type: "DARKMODE",
+  };
+};
+
+const LightMode = () => {
+  return {
+    type: "LIGHTMODE",
+  };
+};
+
+export { DarkMode, LightMode };
