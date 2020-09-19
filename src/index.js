3ee578be9fd219a18c9d811c4258b6f6f55c871e
waiscodes <b.wais@outlook.com>
2020-09-18 19:30:09 -0600

    added template files

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import SignUp from "./components/Signup";
 
 const myStore = createStore();
 
 ReactDOM.render(
   <Provider store={myStore}>
-    <></>
+    <SignUp />
   </Provider>,
   document.getElementById("root")
 );
