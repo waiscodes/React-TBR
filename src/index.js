2a9bc2add7cf68162125643742cd5fe669863c20
waiscodes <b.wais@outlook.com>
2020-09-22 06:53:56 -0600

    removed clutter to build as I needed

--- src/index.js
+++ src/index.js
@@ -3,14 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-// import Header from "./components/Header";
-import LightDarkMode from "./reducers/rLightDarkMode";
+
 import Signup from "./components/Signup";
 
-const myStore = createStore(
-  LightDarkMode,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const myStore = createStore();
 
 ReactDOM.render(
   <Provider store={myStore}>
