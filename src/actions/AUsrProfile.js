68cf055e5c6cfe6d6730090e73e84c14cfe2e4dd
waiscodes <b.wais@outlook.com>
2020-09-22 07:29:57 -0600

    added an action component for profile

---
+++ src/actions/AUsrProfile.js
@@ -0,0 +1,20 @@
+const UsrName = (name) => {
+  return {
+    type: "USR_NAME",
+    payload: name,
+  };
+};
+
+const UsrEmail = (email) => {
+  return {
+    type: "USR_EMAIL",
+    payload: email,
+  };
+};
+
+const UsrPassword = (password) => {
+  return {
+    type: "USR_PASSWORD",
+    payload: password,
+  };
+};
