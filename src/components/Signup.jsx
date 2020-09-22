b4c57228f5f41a6a91fbf82cbcef086cbb245317
waiscodes <b.wais@outlook.com>
2020-09-22 06:36:38 -0600

    added a form with input fields for signing up

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 const Signup = () => {
-  return <></>;
+  return (
+    <>
+      <form>
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" placeholder="Name" />
+
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="email" placeholder="Email" />
+
+        <label htmlFor="password"></label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="password"
+        />
+
+        <input type="submit" value="Signup" />
+      </form>
+    </>
+  );
 };
 
 export default Signup;
