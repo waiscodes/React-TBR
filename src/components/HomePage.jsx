2d41cbd800bafdf2f6444879a2310b699cbebe9a
waiscodes <b.wais@outlook.com>
2020-09-24 07:49:08 -0600

    added a search bar and event handler to prevent default

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,8 +2,16 @@ import React from "react";
 import { connect } from "react-redux";
 
 const HomePage = () => {
+  const SearchEvent = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <>
+      <form onSubmit={SearchEvent}>
+        <label htmlFor="searchbar">Search Bar</label>
+        <input type="search" name="searchbar" id="searchbar" />
+      </form>
       <h2>This is the HomePage</h2>
     </>
   );
