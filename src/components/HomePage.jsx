feeb8cf82359c421eea4c62a708490d3859623cf
waiscodes <b.wais@outlook.com>
2020-09-24 07:41:51 -0600

    added a home page componenet and connected it to the store

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const HomePage = () => {
+  return (
+    <>
+      <h2>This is the HomePage</h2>
+    </>
+  );
+};
+
+export default connect()(HomePage);
