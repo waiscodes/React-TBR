abee6fa2d5b45fd25ff0e645bbeee1b2df8f0ae9
waiscodes <b.wais@outlook.com>
2020-09-25 06:48:46 -0600

    added a books reducer

---
+++ src/reducers/RBooks.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Books = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_BOOKS":
+      return state;
+    default:
+      return state;
+  }
+};
