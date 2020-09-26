fac674a5218a4000fdc76c6977a5005b859f1841
waiscodes <b.wais@outlook.com>
2020-09-26 08:50:15 -0600

    added redux to component

--- src/components/AddNewBook.jsx
+++ src/components/AddNewBook.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { connect } from "react-redux";
 
 const AddNewBook = (props) => {
   return (
@@ -31,4 +32,4 @@ const AddNewBook = (props) => {
   );
 };
 
-export default AddNewBook;
+export default connect()(AddNewBook);
