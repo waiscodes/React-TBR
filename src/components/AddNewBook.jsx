b59e13655c5d5d08d6d43dabcb0394c1f343a833
waiscodes <b.wais@outlook.com>
2020-09-26 08:46:05 -0600

    added inputs for AddNewBook form

--- src/components/AddNewBook.jsx
+++ src/components/AddNewBook.jsx
@@ -3,7 +3,30 @@ import React from "react";
 const AddNewBook = (props) => {
   return (
     <>
-      <form></form>
+      <form>
+        <label htmlFor="title">Title</label>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          placeholder="Enter the Title"
+        />
+
+        <label htmlFor="Author">Author</label>
+        <input
+          type="text"
+          name="Author"
+          id="Author"
+          placeholder="Enter the Author"
+        />
+
+        <label htmlFor="genre">Genre</label>
+        <select name="genre" id="genre">
+          <option value="fantasy">Fantasy</option>
+          <option value="sci-fi">Sci-fi</option>
+          <option value="non-finction">Non-Fiction</option>
+        </select>
+      </form>
     </>
   );
 };
