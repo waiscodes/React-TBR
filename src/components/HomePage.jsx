014803253a5aff3d32e0a32eb4509a1a6397a7ce
waiscodes <b.wais@outlook.com>
2020-09-27 08:23:43 -0600

    added styling to home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import "../css/HomePage.css";
 
 const HomePage = () => {
   const SearchEvent = (e) => {
@@ -20,12 +21,14 @@ const HomePage = () => {
             placeholder="Search"
           />
         </form>
-        <h2>This is the HomePage</h2>
-        <ul>
-          Feed UL
-          {/* This is a UL that the feed will go on. It will be updated via state, and
+        <section className="feed">
+          <h2>This is the HomePage</h2>
+          <ul>
+            Feed UL
+            {/* This is a UL that the feed will go on. It will be updated via state, and
         it'll most likely contain book recommendations, blogs, articles etc. */}
-        </ul>
+          </ul>
+        </section>
       </section>
     </>
   );
