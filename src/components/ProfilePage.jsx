241e9becc59fe97271bd2069fd7344a57ee01362
waiscodes <b.wais@outlook.com>
2020-09-28 07:57:58 -0600

    added place holder values when edit button is not clicked

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -5,15 +5,19 @@ const Profile = () => {
     <>
       <form>
         <label htmlFor="name">Name</label>
+        <p>Full Name</p>
         <input type="text" name="name" id="name" />
 
         <label htmlFor="username">Username</label>
+        <p>UserName</p>
         <input type="text" name="username" id="username" />
 
         <label htmlFor="email">Email</label>
+        <p>Email</p>
         <input type="password" name="password" id="password" />
 
         <label htmlFor="password">password</label>
+        <p>Password</p>
         <input type="password" name="password" id="password" />
 
         <input type="submit" value="Sumbit" />
